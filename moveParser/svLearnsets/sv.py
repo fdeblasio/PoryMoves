da1bc79bc27f8bfa9ac6cd5238ee678f172b4585
Frank <fdeblasio1004@gmail.com>
2024-02-09 13:30:12 -0500

    Added python script for mined SV data

---
+++ moveParser/svLearnsets/sv.py
@@ -0,0 +1,59 @@
+import json
+import re
+# from collections import OrderedDict
+
+#output = "{\n"
+output = {}
+mons = []
+
+def moveToJson(move):
+    return "MOVE_" + re.sub('[- ]', '_', move.upper())
+
+with open("sv.txt", "r+") as file:
+    mons = file.read().split("\n\n")
+
+for mon in mons:
+    lines = mon.split("\n")
+    monName = lines[1]
+    monName = re.sub('( [A-Z]?#\d*)*', '', monName)
+    monName = re.sub('\d* - ', '', monName)
+    monName = re.sub('[ -]', '_', monName)
+    output[monName.upper()] = {
+        "LevelMoves": [],
+        "PreEvoMoves": [],
+        "TMMoves": [],
+        "EggMoves": [],
+        "TutorMoves": []
+        # "EggMoves": []
+    }
+    for line in lines:
+        if line != "" and line[0] == "-":
+            if line[2] == "[":
+                if line[5] == "]":
+                    level = int(line[3:5])
+                    moveName = moveToJson(line[7:])
+                    levelMove = {
+                        "Level": level,
+                        "Move": moveName
+                    }
+                    output[monName.upper()]["LevelMoves"].append(levelMove)
+                elif line[8] == "]":
+                    output[monName.upper()]["TMMoves"].append(moveToJson(line[10:]))
+            else:
+                if "Egg Moves:" in lines and "Reminder:" in lines:
+                    if lines.index(line) < lines.index("Reminder:"):
+                        output[monName.upper()]["EggMoves"].append(moveToJson(line[2:]))
+                    else:
+                        output[monName.upper()]["PreEvoMoves"].append(moveToJson(line[2:]))
+                elif "Egg Moves:" in lines and "Reminder:" not in lines:
+                    output[monName.upper()]["EggMoves"].append(moveToJson(line[2:]))
+                elif "Egg Moves:" not in lines and "Reminder:" in lines:
+                    output[monName.upper()]["PreEvoMoves"].append(moveToJson(line[2:]))
+
+    #output += "  " + monName.upper() + ": {\n"
+    #output += "  },\n"
+
+#output += "}"
+
+with open("sv.json", "w") as file:
+    file.write(json.dumps(output, indent=2))
